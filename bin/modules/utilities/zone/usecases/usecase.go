3438c220182d7cf173627711ae008854295feda6
Mochamad Aufa <mochamadaufa7@gmail.com>
2022-10-07 20:16:00 +0700

    update api list zone

--- bin/modules/utilities/zone/usecases/usecase.go
+++ bin/modules/utilities/zone/usecases/usecase.go
@@ -1,15 +1,13 @@
 package usecases
 
 import (
-	"fmt"
+	"ofa/bin/modules/utilities/zone/helpers"
 	"ofa/bin/modules/utilities/zone/models"
 	res "ofa/bin/pkg/response"
 )
 
 func (opt *Options) GetList(payload *models.ReqGetList) (result res.SendData, err error) {
-	result.Data = payload.Param.Zone
-	var coba = opt.Gorm.FindOne()
-	fmt.Println(coba)
+	result.Data = opt.Gorm.FindManyCommon(helpers.IDLength[payload.Param.Zone], payload.Param.ID)
 
 	return result, nil
 }
